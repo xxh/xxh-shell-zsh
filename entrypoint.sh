5d58c9dc4da8a5f79ec2b05f2a0495e007f857a9
anki-code <anki-code>
2020-03-30 15:18:10 +0300

    XDG

--- entrypoint.sh
+++ entrypoint.sh
@@ -81,9 +81,6 @@ if [[ ! -d $XXH_HOME/.local/share/zsh ]]; then
   mkdir -p $XXH_HOME/.local/share/zsh
 fi
 
-export XDG_CONFIG_HOME=$XXH_HOME/.config
-export HISTFILE=$XXH_HOME/.local/share/zsh/.zsh_history
-
 if [[ $HOMEPATH != '' ]]; then
   homerealpath=`readlink -f $HOMEPATH`
   if [[ -d $homerealpath ]]; then
@@ -96,5 +93,8 @@ if [[ $HOMEPATH != '' ]]; then
 else
   export HOME=$XXH_HOME
 fi
+
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_DATA_HOME=$HOME/.local/share
 
 ./zsh.sh $EXECUTE_FILE "${EXECUTE_COMMAND[@]}"
